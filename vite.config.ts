cbfcb2a721b0cc7b4e5d9e64c503d81112e284d9
mengchao_gan <1657943220@qq.com>
2022-06-25 16:43:38 +0800

    vite modify

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,13 @@ import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue"
 import path from "path";
 
+const resolve = (dir: string) => path.join(__dirname, dir)
+
 export default defineConfig({
   plugins: [vue()],
   resolve: {
     alias: {
-      "@": path.join(__dirname, "src")
+      "@": resolve("src")
     },
   },
   server: {
