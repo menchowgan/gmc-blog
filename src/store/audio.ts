ead936430c4900fdb23ab690cfb5ed255b559402
mengchao_gan <1657943220@qq.com>
2022-07-08 09:46:23 +0800

    改写pinia store为setup函数

--- src/store/audio.ts
+++ src/store/audio.ts
@@ -1,7 +1,28 @@
+import { ref, computed } from "vue"
 import { defineStore } from 'pinia'
 
-export const useAudioStore = defineStore("audio", {
-  state: () => ({
-    curidx: -1
+export const useAudioStore = defineStore("audio", () => {
+  const curidx = ref<number>(-1)
+  const curCount = ref<number>(0)
+
+  const add = () => {
+    curCount.value++
+  }
+
+  const currentCount = computed(() => {
+    return curCount.value
   })
+
+  const $reset = () => {
+    curidx.value = -1
+    curCount.value = 0
+  }
+
+  return {
+    curidx,
+    curCount,
+    currentCount,
+    add,
+    $reset
+  }
 })
--- src/store/user.ts
+++ src/store/user.ts
@@ -1,32 +1,35 @@
+import { ref, computed } from "vue"
 import { defineStore } from 'pinia';
 import { UserModel, PhotoModel } from "../utils/interfaces"
 import { UserManager } from "@/utils/managers"
 
 const userManager = new UserManager()
 
-export const useUserInfoStore = defineStore("userinfo", {
-  state: () => ({
-    userInfo: {} as UserModel
-  }),
-  actions: {
-    async getUserInfo() {
-      const info = await userManager.getSimpleInfo()
-      if (info) {
-        this.userInfo = info as UserModel;
-      }
+export const useUserInfoStore = defineStore("userinfo", () => {
+  const userInfo = ref<UserModel>({})
+
+  const getUserInfo = async () => {
+    const info = await userManager.getSimpleInfo()
+    if (info) {
+      userInfo.value = info as UserModel;
     }
-  },
-  getters: {
-    photos() {
-      let photos: PhotoModel[] = [];
-      if (this.userInfo.photos && this.userInfo.photos.length) {
-        this.userInfo.photos.forEach((item, index) => {
-          if (index < 6) {
-            photos.push(item);
-          }
-        });
-      }
-      return photos
+  }
+
+  const photos = computed(() => {
+    let photos: PhotoModel[] = [];
+    if (userInfo.value.photos && userInfo.value.photos.length) {
+      userInfo.value.photos.forEach((item, index) => {
+        if (index < 6) {
+          photos.push(item);
+        }
+      });
     }
+    return photos
+  })
+
+  return {
+    userInfo,
+    getUserInfo,
+    photos
   }
 })
